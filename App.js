a9bf8bba7d8a77ea17d1934b5eca3712e65c5c6b
carloscasb <54671330+carloscasb@users.noreply.github.com>
2021-10-11 11:51:18 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Preload from "./barb/screens/preload";
+import SignIn from './barb/screens/SignIn';
+// IMPORTAT CONTEXT que ira  ENGLOBAR O APLICATIVO (NavigationContainer)
+import UserContextProfile from './barb/contexts/UserContext';
+
+export default function App() {
+  
+  //VAMOS Criar nossa PILHA que vai receber o retorno do createStackNavigator(pilha de navegação)
+    const Stack = createStackNavigator();
+
+
+//VAMOS Criar Container, o Navegador e as Telas de navegação ENGLOBADA PELO Context
+    return (
+     
+       <UserContextProfile>
+      <NavigationContainer>
+          <Stack.Navigator
+          initialRouteName="Preload" 
+         >
+          <Stack.Screen name="Preload" component={Preload}   options={{headerShown:false}}
+          // EXIBINDO uma tela Preload ---vai ficar um de cada vez (DEFINR A PRIMEIRA EM  <Stack.Navigator initialRouteName="Preload">)
+          />
+           <Stack.Screen name="SignIn" component={SignIn}   options={{headerShown:false}}/>
+          </Stack.Navigator>
+      </NavigationContainer>
+
+   </UserContextProfile>
+    );
+}
