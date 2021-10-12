95207bbe91cf403266a006fb7cecdeccbbdc2799
carloscasb <54671330+carloscasb@users.noreply.github.com>
2021-10-12 11:18:23 -0300

    12102021_1118
    12102021_1118

--- barb/screens/SignIn.js
+++ barb/screens/SignIn.js
@@ -1,17 +1,71 @@
-import React from "react";
-import { View, Text, Image } from "react-native";
+import React, {useState} from "react";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
 import css from '../assets/css/Css'
+import SignInput from "../../componentes/SignInput";
+import { useNavigation } from "@react-navigation/native";
 
-
+import Envelope from "../assets/images/Envelope.png";
+import Cadeado from  "../assets/images/Cadeado.png";
 
 
 export  default  function App1(){
+
+
+   //Função de NAVEGAÇÃO
+      const navigation = useNavigation();
+
+
+   //Função de state
+      const [emailField, setEmailField] = useState('carlos@gloa');
+      const [passwordField, setPasswordField] = useState('');
+
+      
+   //Função de redirecionamento
+
+        const handleSignClick= ()=> { 
+
+        }
+
+   
+         const handleMessageButtonClick= ()=> { 
+            // NAVEGAR POR ROUTER
+            navigation.navigate({name:'SignUp'})
+           
+
+         }
+
     return(
+      
       <View style={css.headerdois}>
-        <Image style={css.header__img} source={require('../assets/images/barber.png')} />
-        <Text >Vai dar tudo Certo</Text>
-        <Text >Com a bença do Senhor</Text>
-        
+      
+         <Image style={css.header__img} source={require('../assets/images/barber.png')} />
+       
+         <View style={css.inputArea}>
+         
+             <SignInput IconPng={Envelope}
+             placeholder="Digite seu Email"
+             value={emailField}
+             onChangeText={t=>setEmailField(t)}
+             /> 
+              
+              <SignInput IconPng={Cadeado}
+              placeholder="Digite Sua Senha"
+              value={passwordField}
+              onChangeText={t=>setPasswordField(t)}
+              password={true}
+              />
+
+           <TouchableOpacity style={css.CustonButton} onPress={handleSignClick} >
+              <Text style={css.button__text}  >Login</Text>
+           </TouchableOpacity> 
+           </View>
+
+           <TouchableOpacity  style={css.CustonMessageButton} onPress={handleMessageButtonClick} >
+            <Text style={css.TextCuston} > Ainda sem cadastro </Text>
+            <Text style={css.TextCustonBold} > Cadastre-se </Text>
+           </TouchableOpacity>
+
         </View>
+    
     );
 }
---
+++ barb/screens/SignUp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export  default  function app2(){
+    return(
+      <View >
+        
+        <Text >Vai dar tudo Certo</Text>
+        <Text >Com a bença do Senhor</Text>
+        </View>
+    );
+}
