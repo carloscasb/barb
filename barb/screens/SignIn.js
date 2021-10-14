a813970432cbfb126a28a3396651d300d423b047
carloscasb <54671330+carloscasb@users.noreply.github.com>
2021-10-13 21:32:51 -0300

    13102021_ATUALIZANDO NAVEGAÇÂO E REQUISIÇÂO
    13102021_ATUALIZANDO NAVEGAÇÂO E REQUISIÇÂO

--- barb/screens/SignIn.js
+++ barb/screens/SignIn.js
@@ -1,39 +1,59 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
 import css from '../assets/css/Css'
 import SignInput from "../../componentes/SignInput";
 import { useNavigation } from "@react-navigation/native";
+import Api from '../Api';
+
 
 import Envelope from "../assets/images/Envelope.png";
 import Cadeado from  "../assets/images/Cadeado.png";
 
 
-export  default  function App1(){
+export  default  function Login(){
 
 
    //Função de NAVEGAÇÃO
-      const navigation = useNavigation();
+   const navigation = useNavigation();
 
 
    //Função de state
-      const [emailField, setEmailField] = useState('carlos@gloa');
+      const [emailField, setEmailField] = useState('');
       const [passwordField, setPasswordField] = useState('');
 
       
    //Função de redirecionamento
 
-        const handleSignClick= ()=> { 
+        const handleSignClick = async ()=> { 
+            // Se apertou botao de login verifivar se colocou email e senha
+            if(emailField !== '' && passwordField !== '' ) {
+               // RECEBER A RESPOSTA (podia ser let res)
+               let json = await Api.signIn(emailField, passwordField );
+               console.log(json);
+               // Verifica A RESPOSTA
+               if (json.token){
+                  alert('DEU CERTO');
 
+               }else{
+                  alert('Email e ou senha errado');
+               }
+
+            }else{
+               alert ('Preencha os campos');
+
+            }
         }
 
    
-         const handleMessageButtonClick= ()=> { 
-            // NAVEGAR POR ROUTER
-            navigation.navigate({name:'SignUp'})
-           
-
+         const handleMessageButtonClick = ()=> { 
+              // // NAVEGAR POR ROUTER (reset - não deixa voltar)
+              navigation.reset({ 
+                index:0,
+               routes:[{name: 'SignUp'}]
+                
+            });
          }
-
+ 
     return(
       
       <View style={css.headerdois}>
@@ -60,7 +80,7 @@ export  default  function App1(){
            </TouchableOpacity> 
            </View>
 
-           <TouchableOpacity  style={css.CustonMessageButton} onPress={handleMessageButtonClick} >
+           <TouchableOpacity  style={css.CustonMessageButton}    onPress={handleMessageButtonClick}    >
             <Text style={css.TextCuston} > Ainda sem cadastro </Text>
             <Text style={css.TextCustonBold} > Cadastre-se </Text>
            </TouchableOpacity>
--- barb/screens/SignUp.js
+++ barb/screens/SignUp.js
@@ -1,12 +1,101 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, {useState, useEffect} from "react";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
+import css from '../assets/css/Css'
+import SignInput from "../../componentes/SignInput";
+import { useNavigation } from "@react-navigation/native";
+import Api from '../Api';
 
-export  default  function app2(){
+import Envelope from "../assets/images/Envelope.png";
+import Cadeado from  "../assets/images/Cadeado.png";
+
+
+export  default  function App2(){
+
+ 
+
+   //Função de NAVEGAÇÃO
+   const navigation=useNavigation();
+
+
+   //Função de state
+      const [nameField, setNameField] = useState('');
+      const [emailField, setEmailField] = useState('');
+      const [passwordField, setPasswordField] = useState('');
+
+      
+   //Função de redirecionamento
+
+        const handleSignClick =  async()=> { 
+         if(nameField !== '' && emailField !== '' && passwordField !== '' ) {
+            // RECEBER A RESPOSTA (podia ser let res)
+            let res = await Api.signUp(nameField, emailField, passwordField );
+            console.log(res);
+            // Verifica A RESPOSTA
+            if (res.token){
+               alert('DEU CERTO');
+
+            }else{
+               alert("Erro :"+res.error);
+            }
+
+         }else{
+            alert ('Preencha os campos');
+
+         }
+        }
+
+   
+         const handleMessageButtonClick = ()=> { 
+             // NAVEGAR POR ROUTER
+            //navigation.navigate('SignUp');
+          // handleMessageButtonClick();
+             navigation.navigate({ 
+               routes:[{name: 'SignIn'}]
+            });
+           /*
+            onPress= ()=>{SignUp}  
+                
+           onPress={()=>{navigation.navigate("Home")}}  */
+         }
+
     return(
-      <View >
-        
-        <Text >Vai dar tudo Certo</Text>
-        <Text >Com a bença do Senhor</Text>
+      
+      <View style={css.headerdois}>
+      
+         <Image style={css.header__img} source={require('../assets/images/barber.png')} />
+       
+         <View style={css.inputArea}>
+
+            <SignInput IconPng={Envelope}
+             placeholder="Digite seu Nome"
+             value={nameField}
+             onChangeText={t=>setNameField(t)}
+             /> 
+
+             <SignInput IconPng={Envelope}
+             placeholder="Digite seu Email"
+             value={emailField}
+             onChangeText={t=>setEmailField(t)}
+             /> 
+              
+              <SignInput IconPng={Cadeado}
+              placeholder="Digite Sua Senha"
+              value={passwordField}
+              onChangeText={t=>setPasswordField(t)}
+              password={true}
+              />
+
+           <TouchableOpacity style={css.CustonButton} onPress={handleSignClick} >
+              <Text style={css.button__text}  >CADASTRAR</Text>
+           </TouchableOpacity> 
+           </View>
+
+           <TouchableOpacity  style={css.CustonMessageButton} onPress={handleMessageButtonClick} >
+            <Text style={css.TextCuston} >Já tem cadastro </Text>
+            <Text style={css.TextCustonBold} > Faça Login </Text>
+           </TouchableOpacity>
+
         </View>
+    
     );
 }
