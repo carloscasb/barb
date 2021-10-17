6c92a34836047f73127808244f55e6f06447341e
carloscasb <54671330+carloscasb@users.noreply.github.com>
2021-10-17 00:05:38 -0300

    Home_161-2-21_ESTLIZANDO
    Home_161-2-21_ESTLIZANDO - sem aceirar incon

--- barb/screens/Home.js
+++ barb/screens/Home.js
@@ -1,17 +1,45 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image } from 'react-native'
 import css from '../assets/css/Css'
+import { Container,
+    Scroller,
+    HeaderArea,
+    HeaderTitle,
+    SearchButton,
+    LocationArea,
+    LocationInput,
+    LocationFinder,
+    LoadingIcon,
+    ListArea
+} from '../assets/css/estilo'
+
+import SearchIcon from '../assets/images/search.png';
+//import MyLocationIcon  from '../assets/images/my_location.png';
+
 
 export default () => {
 
     return (
 
-    <View style = {css.container}>
-        <Text>
-            Home        
-        </Text>
+            <Container>
+                    <Scroller>
+                        <HeaderArea>
+                            <HeaderTitle >Encontre o seu barbeiro favorito</HeaderTitle>
+                                 <SearchButton >
+                                 <Image
+                                 source={require('../assets/images/local.png')}
+                                    />
+                                </SearchButton>
+                        </HeaderArea>
 
-    </View>
+                       
+
+
+                    </Scroller>
+                
+            </Container>
+
+           
 
     );
 
