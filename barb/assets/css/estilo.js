e176d3a7fbc695f33796ba9a0329107f22d9b186
carloscasb <54671330+carloscasb@users.noreply.github.com>
2021-10-29 19:57:28 -0300

    estilos_29102021-CONTINUA BARBER.JS e ESTILOS
    estilos_29102021-CONTINUA BARBER.JS e ESTILOS

--- barb/assets/css/estilo.js
+++ barb/assets/css/estilo.js
@@ -54,3 +54,97 @@ export const ListArea = styled.View`
     margin-top: 30px;
     margin-bottom: 30px;
 `;
+
+export const FakeSwiper = styled.View`
+    
+`;
+
+export const PageBody = styled.View`
+        background-color: #FFFFFF;
+        border-top-left-radius:50px;
+         margin-top:-50px;
+          min-height: 600px;
+
+
+`;
+
+
+
+export const ServiceArea = styled.View`
+    
+`;
+
+export const TestemunhoArea = styled.View`
+    
+`;
+export const SwiperDot = styled.View`
+    width: 15px;
+    height: 15px;
+    border-radius: 5px;
+    background-color: #FFFFFF;
+    margin:3px;
+`;
+
+export const SwiperDotActive = styled.View`
+        width: 15px;
+        height: 15px;
+        border-radius: 5px;
+        background-color: #000000;
+        margin:3px;
+`;
+
+export const SwiperItem = styled.View`
+        flex: 1;
+        background-color: #63C2D1;
+`;
+export const SwiperImage = styled.Image`
+            width: 100%;
+            height: 240px;
+`;
+
+export const UserInfoArea = styled.View`
+        flex-Direction:row;  
+      
+`;
+
+export const UserAvatar = styled.Image`
+            width: 110px;
+            height: 110px;
+            border-Radius: 20px;
+            margin-Left:30px;
+            margin-Height:20px;
+            border-Width:4px;
+            border-Color:'#FFFFFF';
+    
+`;
+
+export const UserInfo = styled.Text`
+            flex: 1;
+            justify-Content:flex-end;
+            margin-Left:30px;
+            
+      
+`;
+
+export const UserInfoName = styled.Text`
+    
+        color:#000000;
+        font-Size:18px;
+        font-Weigth:Bold;
+        margin-Botton:10px;
+        
+  
+    
+`;
+
+export const UserFavButton = styled.TouchableOpacity`
+        width: 40px;
+        height: 40px;
+        border-radius: 20px;
+        background-color: #FFFFFF;
+        border:2px solid #999999;
+        justify-Content: center;
+        align-items: center;
+        
+`;
+
