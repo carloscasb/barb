8794ef4d1eddf2022074d9d316b892a29d357db4
carloscasb <54671330+carloscasb@users.noreply.github.com>
2021-10-29 19:56:01 -0300

    Barber_29102021-CONTINUA BARBER.JS e ESTILOS
    Barber_29102021-CONTINUA BARBER.JS e ESTILOS

--- barb/screens/Barber.js
+++ barb/screens/Barber.js
@@ -1,8 +1,28 @@
 import React, {useState, useEffect} from 'react'
 import { Text, View  } from 'react-native'
 import css from '../assets/css/Css'
+// IMPORTAR estilos
+import estilos, { Container, 
+   Scroller , FakeSwiper,
+   PageBody, UserInfoArea,
+   ServiceArea, TestemunhoArea,
+   SwiperDot, SwiperDotActive,
+   SwiperItem, SwiperImage,
+   UserAvatar, UserInfo,
+   UserInfoName, UserFavButton,
+   LoadingIcon
+  } from '../assets/css/estilo'
+
+// IMPORTAR API
+import Api from '../Api';
 
 import { useNavigation, useRoute } from '@react-navigation/native'
+// IMPORTAR Swiper
+import Swiper from 'react-native-swiper';
+// IMPORTAR Stars - DESPREZAMOS STARS
+import Stars from '../componentes/Stars'    
+// IMPORTAR Imagem de coração
+ import FavoriteIcon from '../assets/images/favorites.png'
 
 export default () => {
 
@@ -23,12 +43,97 @@ export default () => {
 
       });
 
-    return (
-        <View style = {css.container}>
-        <Text> Barber : {userInfo.name}      </Text>
+      //CRIAR UM LOADING
+      const [loading, setLoading]= useState(false);
+    
+      // ASSIM QUE ENTRA NA TELA 
+      useEffect( () => {
+      const getBarberInfo = async () => {
+        //QUANDO INICAR CHAMA O LOADING
+        setLoading(true);
 
-    </View>
-   
+      // PEGA AS INFORMAÇÂO DO PROFISSIONAL
+      let json = await Api.getBarber(userInfo.id);
+      // VERIFICA SE TEM ERRO DO PROFISSIONAL , se não tiver
+      if (json.error == '') {
+        // PEGA TODAS AS INFORMAÇÕES
+        setUserInfo (json.data);
+
+      }else {
+        // SE TIVER ERRO
+        alert("ERRO : "+json.error);
+      }
+        //QUANDO FINALIZAR FECHA O LOADING
+        setLoading(false);
+      }
+
+      getBarberInfo();
+
+      }, []);
+ 
+    return (
+       <Container>
+         <Scroller>
+       
+        {userInfo.photos && userInfo.photos.length > 0 ?
+            <Swiper 
+              style= {{height:240}}
+              dot={<SwiperDot/>}
+              activeDot={<SwiperDotActive/>}
+              paginationStyle={{top:15 , height:15, bottom: null, left:null}}
+              autoplay={true}
+            >
+              {userInfo.photos.map((item, key) => (
+            <SwiperItem key ={key}>
+              <SwiperImage source={{uri:item.url}} resizeMode='cover' />
+            </SwiperItem>
+
+              )) }
+
+            </Swiper>
+            :
+            <FakeSwiper>
+           
+            </FakeSwiper>
+
+         }  
+        
+           <PageBody>
+
+                <UserInfoArea>
+
+                  <UserAvatar source={{uri:userInfo.avatar}}/>
+
+                  <UserInfo>
+                     <UserInfoName>{userInfo.name}</UserInfoName>
+                    
+                  </UserInfo>
+
+                  <UserFavButton>
+                      
+                  </UserFavButton>
+
+                </UserInfoArea>
+                
+                {loading &&  //QUANDO TIVER CARREGANDO
+                <LoadingIcon size="large" color="#000000" />
+                }
+                <ServiceArea>
+                  
+                </ServiceArea>
+
+                <TestemunhoArea>
+
+                </TestemunhoArea>
+
+
+
+
+
+           </PageBody>
+        
+         </Scroller>
+       </Container>
 
     );
 }
